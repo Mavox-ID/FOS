038a950fd1d7608c9f71792e2824961725706b61
Dont Forget All Please <markd.voznyuk@gmail.com>
2026-02-16 12:27:04 +0200

    Add Flip

---
+++ flip.asm
@@ -0,0 +1,17 @@
+[BITS 16]
+[ORG 0x7C00]
+
+start:
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+
+    mov ax, 0x0013
+    int 0x10
+
+    mov ax, 0xA000
+    mov es, ax
